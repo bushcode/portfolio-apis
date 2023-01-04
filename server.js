a3680adb4b014d0d02306f4e81d2d337d5122f1e
Folarin Farinto <folysmith@gmail.com>
2023-01-04 13:00:36 -0500

    updated mongo uri on prod

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const routes = require('./routes/index.routes');
-let db = process.env.DB_URI;
 
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -12,46 +11,63 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.set('trust proxy', 1);
 
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-	);
-	next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-	.connect(db, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log('MongoDB Connected');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+// // Connect to MongoDB
+// mongoose
+//   .connect(db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log('MongoDB Connected');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const connectDB = async () => {
+  let DB_URI;
+
+  process.env.NODE_ENV !== 'production'
+    ? (DB_URI = process.env.DB_URI)
+    : (DB_URI = process.env.PROD_DB_URI);
+  try {
+    const conn = await mongoose.connect(DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 app.use('/', routes);
 app.use(function (req, res, next) {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use(function errorHandler(err, req, res, next) {
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	res.status(err.status || 500).json({ error: res.locals.message });
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500).json({ error: res.locals.message });
 });
 
 const port = process.env.PORT || 3110;
 
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 });
